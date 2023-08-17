ff63265d44b3340badac2fe0968967285fb6638c
sidhuzzle <104455849+sidhuzzle@users.noreply.github.com>
2023-08-17 18:50:23 +0530

    Update cit.py

--- cit.py
+++ cit.py
@@ -1,71 +1,67 @@
-from langchain.llms import OpenAI
-from langchain.chains import LLMChain
-from langchain.prompts import PromptTemplate
-from langchain.chains import SimpleSequentialChain
 import streamlit as st
+from hugchat import hugchat
+import openai
 from streamlit_chat import message
-from langchain.chains import ConversationChain
-from langchain.llms import OpenAI
 
 
-token = "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" 
-llm = OpenAI(temperature=1, openai_api_key=config.token,max_tokens = 1024)
+openai.api_key = st.secret["api_key"]
+
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+if 'messages' not in st.session_state:
+    st.session_state['messages'] = [
+        {"role": "system", "content": "You are a helpful assistant."}
+    ]
+if 'time' not in st.session_state:
+    st.session_state['time'] = []
+if 'utensils' not in st.session_state:
+    st.session_state['utensils'] = []
+
+if 'servings' not in st.session_state:
+    st.session_state['servings'] = 0.0
+
+st.sidebar.title("Filters")
+time = st.sidebar.slider('Cooking time', 0, 100,key = 'one')
+
+utensils =  st.sidebar.multiselect('MAJOR APPLIANCES',['Stove or cooking range','Mixer or food processor','Grinder','Refrigerator','Microwave Oven','Induction stove','Rice cooker'])
+
+servings = st.sidebar.slider('No of Servings', 1, 10,key = 'two')
 
 
+if time == 0 & len(utensils) == 0  & servings == 0:
+    message = 'recipe' + ' ' + 'with' + " " + 'ingredients' + " " + 'and' + " " + 'instruction'
+else:
+    message = 'recipe' + " " +  'within' + "  " + str(time)  + "  " + 'minutes' + "   " + 'using' + "  " + utensils + "  " + 'for' + "  "  + str(servings) +  "  "  + 'people'
+ 
 
+   
+def generate_response(prompt):
+    st.session_state['messages'].append({"role": "user", "content": prompt})
 
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=st.session_state['messages']
+    )
+    response = completion.choices[0].message.content
+    st.session_state['messages'].append({"role": "assistant", "content": response})
+    return response
 
+if prompt := st.chat_input():
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
+        s = ['how much','gm','gram','kilogram','what does','quantity','color','colour','kg','tablespoon','teaspoon','boil','heat','freeze','required']
+        for i in s:
+            if i in prompt.split():
+                user_input = prompt
+            else:
+                user_input = str(prompt) +  " " + str(message)
+        #s = pd.unique(list(user_input))
+        #userinput = ''.join(s)
+        print(user_input)
+        #st.write(user_input)
 
-
-def load_chain_ingredients():
-    template = """Your job is to come up with a recipe with ingredients and instructions from the ingredients list that the users suggests.
-    % INGREDIENTS LIST
-    {ingredients_list}
-
-    YOUR RESPONSE:
-    """
-    prompt_template = PromptTemplate(input_variables=["ingredients_list"], template=template)
-
-    location_chain = LLMChain(llm=llm, prompt=prompt_template)
-    return location_chain
-
-
-
-loc_chain = load_chain_ingredients()
-
-overall_chain = SimpleSequentialChain(chains=[loc_chain], verbose=True)
-
-st.set_page_config(page_title=" ChefInTech", page_icon=":chef:")
-if "generated" not in st.session_state:
-    st.session_state["generated"] = []
-
-if "past" not in st.session_state:
-    st.session_state["past"] = []
-
-
-def get_text():
-    
-    input_text = st.chat_input("", key="input")
-    input_2 = "recipe"
-    input_3 = "ingredients"
-    input_4 = "and"
-    input_5 =  "with"
-    input_6 = "instruction"
-    input_text = " ".join([input_text,input_2,input_5,input_3,input_4,input_5,input_6])
-    
-    return input_text
-    
-
-user_input = get_text()
-
-if user_input:
-    output = overall_chain.run(input=user_input)
-
-    #st.session_state.past.append(user_input)
-    #st.session_state.generated.append(output)
-    st.write(output)
-if st.session_state["generated"]:
-
-    for i in range(len(st.session_state["generated"]) - 1, -1, -1):
-        message(st.session_state["generated"][i], key=str(i))
-        message(st.session_state["past"][i], is_user=True, key=str(i) + "_user")
+        output = generate_response(user_input)
+        st.write(output)
